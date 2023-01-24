36a5b6c0bf9bef6a8a068619c2e66d93fa9a707d
Marcosauras <44006538+Marcosauras@users.noreply.github.com>
2023-01-23 19:10:14 -0500

    fixed issues with saving

--- routes/html.js
+++ routes/html.js
@@ -1,7 +1,6 @@
 // adding all required files
-const express = require("express")
+const router = require("express").Router();
 const path = require("path")
-let router = express.Router()
 
 // this sends all notes to the html file for the user to be able to see all their notes
 router
@@ -10,9 +9,8 @@ router
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
-// if the user plays with the url they will be sent back to the home page
 router
-    .route("*")
+    .route("/*")
     .get((req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'))    
 })
 
